e27a2ba7fb631283c0172c51e55bc55390e3b232
unknown <ryanrich811@gmail.com>
2023-09-15 16:58:20 -0400

    Starting code for student gradebook program in Java

---
+++ studentsGrades/student/src/Student.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Student {
+
+    private String name;
+    private char grade;
+    private ArrayList<String> nameList = new ArrayList<String>();
+    private ArrayList<char> gradeList = new ArrayList<char>();
+
+    public static void main(String[] args) {
+
+    }
+}
