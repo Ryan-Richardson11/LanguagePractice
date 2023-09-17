bdb4c6e3a619237ba1d20f22e937a4f32dbd0352
unknown <ryanrich811@gmail.com>
2023-09-17 16:17:52 -0400

    Display account details method created

--- bank/bank/src/Bank.java
+++ bank/bank/src/Bank.java
@@ -16,7 +16,7 @@ public class Bank {
         this.accountBalance = accountBalance;
     }
 
-    void addAccount(ArrayList<Bank>account){
+    void addAccount(ArrayList<Bank>accounts){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter the account holder's name: ");
         name = input.nextLine();
@@ -24,13 +24,41 @@ public class Bank {
         accountType = input.nextLine();
         Random random = new Random();
         int accountNumber = random.nextInt(1000);
-        account.add(name, accountNumber, accountType, accountBalance);
+        accounts.add(new Bank(name, accountNumber, accountType, accountBalance));
+    }
+
+    int getAccountNumber(){
+        return this.accountNumber;
+    }
+
+    String getName(){
+        return this.name;
+    }
+
+    String getAccountType(){
+        return this.accountType;
     }
 
     void deposit(){
         Scanner input = new Scanner(System.in);
         System.out.println("Enter and amount to deposit: ");
         this.accountBalance += input.nextDouble();
+    }
+
+    void withdraw(){
+
+    }
+
+    void accountDetails(ArrayList<Bank>accounts, int accountNumber){
+        for (Bank account : accounts){
+            if (accountNumber == account.getAccountNumber()){
+                System.out.println("Account Details: ");
+                System.out.println("Name: " + account.getName());
+                System.out.println("Account Type: " + account.accountType);
+                System.out.println("Balance: " + accountBalance);
+            }
+        System.out.println("Account number not found.");    
+        }
     }
         
 
