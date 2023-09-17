daf5e9e2896b849851c6d8e0e24be41fb13f4a79
unknown <ryanrich811@gmail.com>
2023-09-17 14:40:35 -0400

    Stubbed out code for library program

---
+++ library/library/src/Book.java
@@ -0,0 +1,52 @@
+import java.util.ArrayList;
+
+public class Book{
+
+    private String title;
+    private String author;
+    private char availability;
+
+    public Book(String title, String author, char availability){
+        this.title = title;
+        this.author = author;
+        this.availability = availability;
+    }
+
+    String getTitle(){
+        return this.title;
+    }
+
+    String getAuthor(){
+        return this.author;
+    }
+
+    char checkAvailability(){
+        return this.availability;
+    }
+
+    void makeAvailable(){
+        if (this.availability == 'U'){
+            this.availability = 'A';
+        }
+    }
+
+    void makeUnavailable(){
+        if (this.availability == 'U'){
+            this.availability = 'A';
+        }
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Book> library = new ArrayList<>();
+
+        // Create student instances and add them to the list
+        library.add(new Book("To Kill a Mockingbird", "Harper Lee", 'A'));
+        library.add(new Book("1984", "George Orwell", 'U'));
+        library.add(new Book("The Great Gatsby", "F. Scott Fitzgerald", 'A'));
+
+        System.out.println("Books in the Library: ");
+        for ( Book book : library){
+            System.out.println(book.getTitle() + " " + book.getAuthor() + " " + book.checkAvailability());
+        }
+    }
+}
