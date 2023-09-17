0f59f3480446ee0cce714bb200fa1c352d07fde1
unknown <ryanrich811@gmail.com>
2023-09-17 14:01:00 -0400

    Changed student class. Using only one arrayList and storing

--- studentsGrades/student/src/Student.java
+++ studentsGrades/student/src/Student.java
@@ -1,46 +1,43 @@
 import java.util.ArrayList;
 
 public class Student {
-
     private String name;
     private int grade;
-    private ArrayList<String> nameList = new ArrayList<String>();
-    private ArrayList<Integer> gradeList = new ArrayList<Integer>();
 
-    public Student(String name, int grade){
+    public Student(String name, int grade) {
         this.name = name;
         this.grade = grade;
     }
 
-    void addStudent(String name, int grade) {
-        nameList.add(name);
-        gradeList.add(grade);
+    public String getName() {
+        return name;
     }
 
-    int averageGrade(){
-        int total = 0;
-        int count = 0;
-        for (int i = 0; i < gradeList.size(); i++){
-            total += gradeList.get(i);
-            count ++;
-        }
-        return total / count;
-    }
-
-    void displayList(){
-        for (int i = 0; i < nameList.size(); i++){
-            for (int j = 0; j < gradeList.size(); j++){
-                System.out.println(nameList.get(i) + " " + gradeList.get(j));
-            }
-        }
+    public int getGrade() {
+        return grade;
     }
 
     public static void main(String[] args) {
-        Student myStudents = new Student("Ryan", 85);
-        myStudents.addStudent("Sydnie", 90);
-        myStudents.addStudent("Jim", 77);
-        myStudents.displayList();
-        double average = myStudents.averageGrade();
-        System.out.println("The classes average grade is " + average);
+        ArrayList<Student> studentList = new ArrayList<>();
+
+        // Create student instances and add them to the list
+        studentList.add(new Student("Ryan", 85));
+        studentList.add(new Student("Sydnie", 90));
+        studentList.add(new Student("Jim", 77));
+
+        // Display the list of students and their grades
+        System.out.println("Student List:");
+        for (Student student : studentList) {
+            System.out.println(student.getName() + ": " + student.getGrade());
+        }
+
+        // Calculate and display the average grade
+        double totalGrade = 0.0;
+        for (Student student : studentList) {
+            totalGrade += student.getGrade();
+        }
+        double average = totalGrade / studentList.size();
+        System.out.println("The class's average grade is " + average);
     }
 }
+
