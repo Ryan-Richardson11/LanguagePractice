c0e8e4ad52be54c807e707dd1d25689b4950c343
unknown <ryanrich811@gmail.com>
2023-09-17 13:45:49 -0400

    added averageGrade method and displayList()

--- studentsGrades/student/src/Student.java
+++ studentsGrades/student/src/Student.java
@@ -4,13 +4,36 @@ import java.util.ArrayList;
 public class Student {
 
     private String name;
-    private char grade;
+    private int grade;
     private ArrayList<String> nameList = new ArrayList<String>();
-    private ArrayList<char> gradeList = new ArrayList<char>();
+    private ArrayList<Integer> gradeList = new ArrayList<Integer>();
 
-    void addStudent(String name, char grade) {
+    public Student(String name, int grade){
+        this.name = name;
+        this.grade = grade;
+    }
+
+    void addStudent(String name, int grade) {
         nameList.add(name);
         gradeList.add(grade);
+    }
+
+    int averageGrade(){
+        int total = 0;
+        int count = 0;
+        for (int i = 0; i < gradeList; i++){
+            total += gradeList[i];
+            count += 1;
+        }
+        return total / count;
+    }
+
+    void displayList(){
+        for (int i = 0; i < nameList; i++){
+            for (int j = 0; j < gradeList; j++){
+                System.out.println(i + " " + j +"\n");
+            }
+        }
     }
 
     public static void main(String[] args) {
