64b9809e6135013577791348a2707b6b945cff76
unknown <ryanrich811@gmail.com>
2023-09-17 15:29:51 -0400

    Created constructor for bank program

---
+++ bank/bank/src/Bank.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class Bank {
+
+    private String name;
+    private int accountNumber;
+    private String accountType;
+    private double accountBalance;
+
+    public Bank(String name, int accountNumber, String accountType, double accountBalance){
+        this.name = name;
+        this.accountNumber = accountNumber;
+        this.accountType = accountType;
+        this.accountBalance = accountBalance;
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
