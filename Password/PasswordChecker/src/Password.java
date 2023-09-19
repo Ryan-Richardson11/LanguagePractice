807f969acbb93fcfab03c7f7dbbe69be67c12743
Ryan Richardson <ryanrich811@gmail.com>
2023-09-19 12:29:52 -0400

    Starting code for Password Checking program

---
+++ Password/PasswordChecker/src/Password.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Password {
+
+    String createPassword() {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a password: ");
+        String password = input.nextLine();
+        return password;
+    }
+
+    void checkPassword(String password) {
+        for (int i = 0; i < password.length(); i++) {
+            if 
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
