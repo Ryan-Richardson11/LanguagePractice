eeb7befe71e490ddb51b0b754f3b498f8500f0f0
Ryan Richardson <ryanrich811@gmail.com>
2023-09-22 11:38:57 -0400

    Completed code with working logic for creating password, checking validity, and outputting the correct strength level

--- Password/PasswordChecker/src/Password.java
+++ Password/PasswordChecker/src/Password.java
@@ -4,42 +4,51 @@ public class Password {
 
     String createPassword() {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter a password: ");
+        System.out.println("Enter a password ('Exit' to exit): ");
         String password = input.nextLine();
         return password;
     }
 
     void checkPassword(String password) {
+
         if (password.length() >= 8) {
+            boolean hasUppercase = false;
+            boolean hasLowercase = false;
+            boolean hasDigit = false;
+
             for (int i = 0; i < password.length(); i++) {
                 if (Character.isUpperCase(password.charAt(i))) {
-                    continue;
-                }
-                if (Character.isLowerCase(password.charAt(i))) {
-                    continue;
-                }
-                if (Character.isDigit(password.charAt(i))) {
-                    continue;
-                }
-                if (password.length() >= 8 && password.length() <= 11) {
-                    System.out.println("Password strength: Weak");
-                    break;
-                }
-                if (password.length() >= 12 && password.length() <= 14) {
-                    System.out.println("Password strength: Medium");
-                    break;
-                } else {
-                    System.out.println("Password strength: Strong");
-                    break;
+                    hasUppercase = true;
+                } else if (Character.isLowerCase(password.charAt(i))) {
+                    hasLowercase = true;
+                } else if (Character.isDigit(password.charAt(i))) {
+                    hasDigit = true;
                 }
             }
-            System.out.println("Invalid Password");
+            if (hasUppercase && hasLowercase && hasDigit == true) {
+                if (password.length() >= 8 && password.length() <= 11) {
+                    System.out.println("Password strength: Weak");
+                } else if (password.length() >= 12 && password.length() <= 14) {
+                    System.out.println("Password strength: Medium");
+                } else {
+                    System.out.println("Password strength: Strong");
+                }
+            } else {
+                System.out.println("Invalid Password");
+
+            }
         }
     }
 
     public static void main(String[] args) {
         Password password = new Password();
-        String ans = password.createPassword();
-        password.checkPassword(ans);
+        while (true) {
+            String ans = password.createPassword();
+            if (ans.equals("Exit")) {
+                break;
+            } else {
+                password.checkPassword(ans);
+            }
+        }
     }
 }
