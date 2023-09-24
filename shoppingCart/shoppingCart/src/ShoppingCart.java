ee177d2cb6f4b5c6c6c41e47d5d0123120285efd
unknown <ryanrich811@gmail.com>
2023-09-24 19:59:35 -0400

    addProdcut() function implemented in Product class

--- shoppingCart/shoppingCart/src/ShoppingCart.java
+++ shoppingCart/shoppingCart/src/ShoppingCart.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ShoppingCart {
 
     public class Product {
@@ -5,10 +8,10 @@ public class ShoppingCart {
         double price;
         int quantity;
 
-        public Product() {
-            String productName;
-            double price;
-            int quantity;
+        public Product(String productName, double price, int quantity) {
+            this.productName = productName;
+            this.price = price;
+            this.quantity = quantity;
         }
 
         String getProductName() {
@@ -22,9 +25,7 @@ public class ShoppingCart {
         int getQuantity() {
             return quantity;
         }
-    }
 
-    public class Cart extends Product {
         void displayOptions() {
             System.out.println("Enter 1 to add a product: ");
             System.out.println("Enter 2 to remove a product: ");
@@ -32,9 +33,19 @@ public class ShoppingCart {
             System.out.println("Enter 4 to view the total cost: ");
             System.out.println("Enter 5 to exit the program: ");
         }
+
+        void addProduct(ArrayList<Product>) {
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter a product name: ");
+            String productName = input.nextLine();
+            System.out.println("Enter a product price: ");
+            double price = input.nextDouble();
+            System.out.prinln("Enter the product quantity: ");
+            int quantity = input.nextInt();
+            ArrayList.add(Product(productName, price, quantity));
     }
 
     public static void main(String[] args) {
-
+        ArrayList<Product> cart = new ArrayList<>();
     }
 }
