24f84b4a342ac744e7f947bc28d12891ee271fc3
unknown <ryanrich811@gmail.com>
2023-10-16 15:52:53 -0400

    First iteration of createFile method

---
+++ fileHandler/fileManager/src/file.java
@@ -0,0 +1,29 @@
+import java.io.File;
+
+public class file {
+
+    void createFile() {
+
+    }
+
+    void deleteFile() {
+
+    }
+
+    void moveFile() {
+
+    }
+
+    void searchFile() {
+
+    }
+
+    void displayDirectory() {
+
+    }
+
+    void fileContents() {
+
+    }
+}
+
