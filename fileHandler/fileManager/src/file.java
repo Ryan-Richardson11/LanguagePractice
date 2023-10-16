d17bb3801f6eb5ea0b6d98f368615cb4d3cae2d7
unknown <ryanrich811@gmail.com>
2023-10-16 16:05:59 -0400

    First iteration of deleteFile method

--- fileHandler/fileManager/src/file.java
+++ fileHandler/fileManager/src/file.java
@@ -1,13 +1,30 @@
 import java.io.File;
+import java.util.Scanner;
 
 public class file {
 
     void createFile() {
+        System.out.println("Enter a file to create: ");
+        Scanner input = new Scanner(System.in);
+        String newFile = input.nextLine();
+        input.close();
+        File myFile = new File(newFile);
+        if (myFile.createNewFile()) {
+            System.out.println("File " + newFile + " was created");
+        }
+        else {
+            System.out.println("File could not be created");
+        }
 
     }
 
     void deleteFile() {
-
+        System.out.println("Enter your files full path: ");
+        Scanner input = new Scanner(System.in);
+        String fileToDelete = input.nextLine();
+        input.close();
+        fileToDelete.delete();
+        System.out.println("The file has been deleted.");
     }
 
     void moveFile() {
@@ -25,5 +42,8 @@ public class file {
     void fileContents() {
 
     }
+
 }
+
+
 
