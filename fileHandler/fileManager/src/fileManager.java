bddfb328b1b8230a83a2140ae227d9c63d5c32d4
unknown <ryanrich811@gmail.com>
2023-10-24 18:02:24 -0400

    Completed main method for program entry and flow control

--- fileHandler/fileManager/src/fileManager.java
+++ fileHandler/fileManager/src/fileManager.java
@@ -1,3 +1,5 @@
+
+import java.util.Scanner;
 public class fileManager {
     public static void main(String[] args) {
         while (true) {
@@ -7,7 +9,28 @@ public class fileManager {
             System.out.println("3: Move a file");
             System.out.println("4: Search a file");
             System.out.println("5: Display a directory");
-            System.out.println("Display file contents");
+            System.out.println("6: Display file contents");
+            Scanner scanner = new Scanner(System.in);
+            int choice = scanner.nextInt();
+            file myFile = new file();
+            if (choice == 1) {
+                myFile.createFile();
+            }
+            else if (choice == 2) {
+                myFile.deleteFile();
+            }
+            else if (choice == 3) {
+                myFile.moveFile();
+            }
+            else if (choice == 4) {
+                myFile.searchFile();
+            }
+            else if (choice == 5) {
+                myFile.displayDirectory();
+            }
+            else if (choice == 6) {
+                myFile.fileContents();
+            }
         }
     }
 }
