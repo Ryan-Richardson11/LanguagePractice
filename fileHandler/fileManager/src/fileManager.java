308a8d6e0f9ba215194e017f6f4943608bd520a3
unknown <ryanrich811@gmail.com>
2023-10-24 17:55:14 -0400

    Added print menu screen to main

--- fileHandler/fileManager/src/fileManager.java
+++ fileHandler/fileManager/src/fileManager.java
@@ -1,5 +1,13 @@
 public class fileManager {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("What task would you like to complete?");
+            System.out.println("1: Create a file");
+            System.out.println("2: Delete a file");
+            System.out.println("3: Move a file");
+            System.out.println("4: Search a file");
+            System.out.println("5: Display a directory");
+            System.out.println("Display file contents");
+        }
     }
 }
