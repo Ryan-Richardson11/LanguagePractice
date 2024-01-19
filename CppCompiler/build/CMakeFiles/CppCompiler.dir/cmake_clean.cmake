b06b6987ff1c21f457f63a7f6c86437618712793
Ryan Richardson <ryanrich811@gmail.com>
2024-01-19 13:27:54 -0500

    Converted to Cmake

---
+++ CppCompiler/build/CMakeFiles/CppCompiler.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppCompiler.dir/src/lexer.cpp.obj"
+  "CMakeFiles/CppCompiler.dir/src/lexer.cpp.obj.d"
+  "CMakeFiles/CppCompiler.dir/src/parser.cpp.obj"
+  "CMakeFiles/CppCompiler.dir/src/parser.cpp.obj.d"
+  "CppCompiler.exe"
+  "CppCompiler.exe.manifest"
+  "CppCompiler.pdb"
+  "libCppCompiler.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppCompiler.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CppCompiler/build/CMakeFiles/CppCompiler.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for CppCompiler.
+# This may be replaced when dependencies are built.
