0dd4354e3ef64f3767cef6e30f7a91b7c1a0c0c8
Ryan Richardson <ryanrich811@gmail.com>
2024-05-15 21:07:07 -0400

    New class attributes

--- Encrypt/encryption_functions.py
+++ Encrypt/encryption_functions.py
@@ -2,9 +2,17 @@ import hashlib
 
 
 class Encrypt:
-    def __init__(self, file_name, file_path=None):
+    def __init__(self, file_name, file_path=None, password=None, dir=None):
         self.file_name = file_name
         self.file_path = file_path
+        self.password = password
+        self.dir = dir
 
-    def encrypt_file(file_name):
+    def encrypt_file(self, file_name):
+        pass
+
+    def encrypt_password(self, password):
+        pass
+
+    def encrypt_dir():
         pass
