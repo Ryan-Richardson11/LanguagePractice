5fd5214b07f638df420a92f841c13166a7da4fbb
Ryan Richardson <ryanrich811@gmail.com>
2024-05-15 20:53:18 -0400

    Starting class for file encryption

---
+++ Encrypt/encryption_functions.py
@@ -0,0 +1,10 @@
+import hashlib
+
+
+class Encrypt:
+    def __init__(self, file_name, file_path=None):
+        self.file_name = file_name
+        self.file_path = file_path
+
+    def encrypt_file(file_name):
+        pass
