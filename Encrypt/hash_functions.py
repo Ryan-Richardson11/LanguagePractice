db444cbe8c36737dd6c1cd47f5f00e7abf73a213
Ryan Richardson <ryanrich811@gmail.com>
2024-06-16 20:58:57 -0400

    Removed render function from Hash class

--- Encrypt/hash_functions.py
+++ Encrypt/hash_functions.py
@@ -14,18 +14,3 @@ class Hash:
             f"{password}".encode(), usedforsecurity=True).hexdigest()
         print(hashed)
         return hashed
-
-    def render_password_hash(self):
-        current = Hash()
-        window = Tk()
-        window.geometry('600x400')
-
-        password_entry = Entry(window, width=30)
-        password_entry.pack()
-        password = password_entry.get()
-        hashed = current.hash_password(password)
-
-        Button(window, text="Hash password",
-               command=hashed).pack()
-
-        window.mainloop()
