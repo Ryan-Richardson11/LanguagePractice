60ecb58e72337d01a71cf6f14f26758ba9e653ce
Ryan Richardson <ryanrich811@gmail.com>
2024-06-19 11:17:52 -0400

    Completed all options for hashing algorithm labels

--- Encrypt/main.py
+++ Encrypt/main.py
@@ -42,31 +42,43 @@ def render_password_hash_page():
     # SHA-2
     hashing_options_frame_sha2 = LabelFrame(
         frame, text="SHA-2 Hashing Algorithms")
-    hashing_options_frame_sha2.grid(row=0, column=2)
+    hashing_options_frame_sha2.grid(row=0, column=1)
     # SHA-3
     hashing_options_frame_sha3 = LabelFrame(
         frame, text="SHA-3 Hashing Algorithms")
-    hashing_options_frame_sha3.grid(row=0, column=3)
+    hashing_options_frame_sha3.grid(row=0, column=2)
     # MD5
     hashing_options_frame_md5 = LabelFrame(frame, text="MD5 Hashing Algorithm")
-    hashing_options_frame_md5.grid(row=0, column=4)
+    hashing_options_frame_md5.grid(row=0, column=3)
 
     # Hashing option labels
     sha1_label = Label(hashing_options_frame_sha1, text="SHA-1")
     sha1_label.grid(row=0, column=0)
 
-    # sha2_224_label =
-    # sha2_256_label =
-    # sha2_512_label =
+    sha2_224_label = Label(hashing_options_frame_sha2, text="SHA-2 (224)")
+    sha2_224_label.grid(row=0, column=1)
+    sha2_256_label = Label(hashing_options_frame_sha2, text="SHA-2 (256)")
+    sha2_256_label.grid(row=1, column=1)
+    sha2_512_label = Label(hashing_options_frame_sha2, text="SHA-2 (512)")
+    sha2_512_label.grid(row=2, column=1)
 
-    # sha3_224_label =
-    # sha3_256_label =
-    # sha3_384_label =
-    # sha3_512_label =
-    # sha3_shake128_label =
-    # sha3_shake256_label =
+    sha3_224_label = Label(hashing_options_frame_sha3, text="SHA-3 (224)")
+    sha3_224_label.grid(row=0, column=2)
+    sha3_256_label = Label(hashing_options_frame_sha3, text="SHA-3 (256)")
+    sha3_256_label.grid(row=1, column=2)
+    sha3_384_label = Label(hashing_options_frame_sha3, text="SHA-3 (384)")
+    sha3_384_label.grid(row=2, column=2)
+    sha3_512_label = Label(hashing_options_frame_sha3, text="SHA-3 (512)")
+    sha3_512_label.grid(row=3, column=2)
+    sha3_shake128_label = Label(
+        hashing_options_frame_sha3, text="SHA-3 (Shake-128)")
+    sha3_shake128_label.grid(row=4, column=2)
+    sha3_shake256_label = Label(
+        hashing_options_frame_sha3, text="SHA-3 (Shake-256)")
+    sha3_shake256_label.grid(row=5, column=2)
 
-    # md5_label =
+    md5_label = Label(hashing_options_frame_md5, text="MD5")
+    md5_label.grid(row=0, column=3)
 
     window.mainloop()
 
