b0216b14f212151fa4f52e98bb7723890483a409
Ryan Richardson <ryanrich811@gmail.com>
2024-09-04 20:45:58 -0400

    added new utility function to generate 16 character salt to be appended to hash

--- Encrypt/src/utils.py
+++ Encrypt/src/utils.py
@@ -1,8 +1,23 @@
 from tkinter import *
 from tkinter import ttk
+import string
+import random
 
 
 def add_tab(notebook, tab_name):
     new_tab = Frame(notebook)
     notebook.add(new_tab, text=f"{tab_name}")
     return new_tab
+
+
+def add_salt():
+    salt = "".join(random.SystemRandom().choice(
+        string.ascii_letters + string.digits) for i in range(16))
+    return salt
+
+
+def main():
+    print(add_salt())
+
+
+main()
