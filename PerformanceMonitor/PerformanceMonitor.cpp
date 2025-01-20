aab999a78a064e113e93559d817b79eba264cbb6
Ryan Richardson <ryanrich811@gmail.com>
2025-01-20 10:09:59 -0500

    Starting files fro C++ performance monitor

---
+++ PerformanceMonitor/PerformanceMonitor.cpp
@@ -0,0 +1,12 @@
+// C++ Performance Monitor
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i <= 5; i++)
+    {
+        cout << "Hello, world!" << endl;
+    }
+    return 0;
+}
