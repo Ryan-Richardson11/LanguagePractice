0fb6d3f70338859fa871d818253ad6f14cba340c
Ryan Richardson <ryanrich811@gmail.com>
2025-01-26 18:17:55 -0500

    Starting class for Performance monitor

--- PerformanceMonitor/PerformanceMonitor.cpp
+++ PerformanceMonitor/PerformanceMonitor.cpp
@@ -2,6 +2,41 @@
 #include <iostream>
 using namespace std;
 
+class PerformanceStats
+{
+public:
+    int MemUsage;
+    int CpuUsage;
+    int DiskUsage;
+    int GpuUsage;
+    int NetworkUsage;
+
+    void GetMemUsage()
+    {
+        cout << "Mem Usage" << endl;
+    }
+
+    void GetCpuUsage()
+    {
+        cout << "CPU Usage" << endl;
+    }
+
+    void GetDiskUsage()
+    {
+        cout << "Disk Usage" << endl;
+    }
+
+    void GetGpuUsage()
+    {
+        cout << "GPU Usage" << endl;
+    }
+
+    void GetNetworkUsage()
+    {
+        cout << "Network Usage" << endl;
+    }
+};
+
 int main()
 {
     for (int i = 0; i <= 5; i++)
